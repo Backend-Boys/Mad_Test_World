5b02fa7427cbc0d17a3b1f08ea33c2772262232e
mad <madcrafty@icloud.com>
2021-05-27 16:49:20 +1000

    init

---
+++ Assets/Scripts/TeleporterActivate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleporterActivate : MonoBehaviour
+{
+    // Access the GameObject that contains the teleport controller script.
+    public GameObject teleportController;
+
+    // Reference to the Input Action Reference that contains the button mapping 
+    // data for activation.
+    public InputActionReference teleportActivateReference;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
